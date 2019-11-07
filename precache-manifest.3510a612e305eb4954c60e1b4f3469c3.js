6f5736e51cd1d69b83f9666cdd22b6ed3b08e571
Sijey <Popov040391@gmail.com>
2019-11-07 13:42:30 +0200

    Updates

---
+++ precache-manifest.3510a612e305eb4954c60e1b4f3469c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2326994cf87477eb9625f511edfc69",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "d7663de04b6a2b23e765",
+    "url": "/react_tabs-with-router/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "fed82e82a74ba6489b1f",
+    "url": "/react_tabs-with-router/static/js/2.db414a40.chunk.js"
+  },
+  {
+    "revision": "d7663de04b6a2b23e765",
+    "url": "/react_tabs-with-router/static/js/main.60f8a823.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
